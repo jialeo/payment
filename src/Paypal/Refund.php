5ea250005260dd3324db0c4ca7a74202ad10ae48
lingxiang <lingxiang@han-zi.cn>
2017-09-05 17:45:16 +0800

    删除注释

--- src/Paypal/Refund.php
+++ src/Paypal/Refund.php
@@ -7,59 +7,6 @@ class Refund extends BasePaypalPay
 {
     /**
      * PayPal退款
-     * return 
-     * array(8) {
-     *  ["id"]=>
-     *  string(17) "4M970811EP7222338"
-     *  ["create_time"]=>
-     *  string(20) "2017-08-14T06:35:14Z"
-     *  ["update_time"]=>
-     *  string(20) "2017-08-14T06:35:14Z"
-     *  ["state"]=>
-     *  string(9) "completed"
-     *  ["amount"]=>
-     *  array(2) {
-     *    ["total"]=>
-     *    string(6) "798.00"
-     *    ["currency"]=>
-     *    string(3) "USD"
-     *  }
-     *  ["sale_id"]=>
-     *  string(17) "9HS19516C7516141J"
-     *  ["parent_payment"]=>
-     *  string(28) "PAY-2EE21012B68178407LGGHG6Y"
-     *  ["links"]=>
-     *  array(3) {
-     *    [0]=>
-     *    array(3) {
-     *      ["href"]=>
-     *      string(67) "https://api.sandbox.paypal.com/v1/payments/refund/4M970811EP7222338"
-     *      ["rel"]=>
-     *      string(4) "self"
-     *      ["method"]=>
-     *      string(3) "GET"
-     *    }
-     *    [1]=>
-     *    array(3) {
-     *      ["href"]=>
-     *      string(79) "https://api.sandbox.paypal.com/v1/payments/payment/PAY-2EE21012B68178407LGGHG6Y"
-     *      ["rel"]=>
-     *      string(14) "parent_payment"
-     *      ["method"]=>
-     *      string(3) "GET"
-     *    }
-     *    [2]=>
-     *    array(3) {
-     *      ["href"]=>
-     *      string(65) "https://api.sandbox.paypal.com/v1/payments/sale/9HS19516C7516141J"
-     *      ["rel"]=>
-     *      string(4) "sale"
-     *      ["method"]=>
-     *      string(3) "GET"
-     *    }
-     *  }
-     *}
-     *
      * @param string $txn_id //交易流水ID
      * @return array                [description]
      */
